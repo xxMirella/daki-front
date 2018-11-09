743c1bbdb8d0a42d411306095c10075fd30f943d
Avanade <Avanade@WIN-FRCWARYBEU.corp.avanade.org>
2018-11-09 17:25:20 -0200

    Card created

---
+++ daki-front/src/app/card/card.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  constructor(
+    private type, 
+    id, 
+    firstLine, 
+    secondLine, 
+    liked, 
+    favorited) { }
+    
+
+
+
+  ngOnInit() {
+  }
+
+}
