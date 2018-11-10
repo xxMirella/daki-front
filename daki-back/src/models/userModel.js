9be381366343df0889cc2918b92f856f01c7d322
ainou <mirella.mih1006@gmail.com>
2018-11-10 15:44:08 -0200

    Adjusted user model.

--- daki-back/src/models/userModel.js
+++ daki-back/src/models/userModel.js
@@ -1,12 +1,10 @@
 'use strict';
 const mongoose = require('mongoose');
-const media = require('./mediaModel');
-const district = require('./districtModel');
 const schema = mongoose.Schema;
 
 const UserModel = new schema({
-  profilePhoto: media,
-  district:     district,
+  profilePhoto: { type: String },
+  district:     { type: String, required: true },
   name:         { type: String, required: true },
   birthDay:     { type: Date, required: true },
   email:        { type: String, required: true, index: { unique: true } },
