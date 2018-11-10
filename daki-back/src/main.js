483dd87d6cc69bb47247e28340e88c1480bc30f9
ainou <mirella.mih1006@gmail.com>
2018-11-10 15:46:03 -0200

    Included authRout to server init.

--- daki-back/src/main.js
+++ daki-back/src/main.js
@@ -1,12 +1,24 @@
 const databaseMongo = require('./config/database');
 const serverHapi = require('./config/server');
 const config = require('./config/config');
+const auth = require('./routes/authRoute');
 
 async function main() {
   const db = new databaseMongo(config.dbConfig.HOST);
   await db.connect();
-  const server = new serverHapi(config.serverConfig.PORT, config.serverConfig.HOST);
-  await server.init();
+
+  const server = new serverHapi(
+    config.serverConfig.PORT,
+    config.serverConfig.HOST,
+    config.TokenKey.key
+  );
+
+  const routes = [
+    auth.login(),
+    auth.signUP()
+  ];
+
+  await server.init(routes);
 }
 
 main();
