be709a58adcf4c9fcdc1d5d6ce072894e07a6846
Avanade <Avanade@WIN-F976TKXQ1J.corp.avanade.org>
2018-11-10 08:00:18 -0200

    front login

--- daki-front/src/app/app.module.ts
+++ daki-front/src/app/app.module.ts
@@ -6,18 +6,22 @@ import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const appRoutes: Routes = [{
   path: '', component: HomeComponent,
- }, {
+}, {
   path: 'register', component: RegisterComponent
- }];
+}, {
+  path: 'login', component: LoginComponent
+}];
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
