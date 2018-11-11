7150e13028d8977c264dffe2f2e3c2261043e6f6
Avanade <Avanade@WIN-5WHYTEO0B4.corp.avanade.org>
2018-11-11 09:51:31 -0200

    add img in post view

--- daki-front/src/app/post-item/post-item.component.ts
+++ daki-front/src/app/post-item/post-item.component.ts
@@ -6,11 +6,20 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./post-item.component.css']
 })
 export class PostItemComponent implements OnInit {
-
+  private image;
   @Input() post: Post;
   constructor() { }
 
   ngOnInit() {
   }
 
+  isValidImg(post) {
+    return post.foto.value;
+  }
+
+  getImage(post) {
+    this.image = `data:${post.foto.filetype};base64,${post.foto.value}`
+    console.log(this.image + " " + (post.foto.value))
+    return this.image;
+  }
 }
